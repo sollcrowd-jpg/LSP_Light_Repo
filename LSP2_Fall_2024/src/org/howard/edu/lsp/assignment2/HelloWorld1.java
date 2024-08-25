8e88d7c5dd4b1c59a8bbafc7f1b0b7620759451a
sollo <sollo@localhost>
2024-08-25 02:24:23 -0500

    First Commit

---
+++ LSP2_Fall_2024/src/org/howard/edu/lsp/assignment2/HelloWorld1.java
@@ -0,0 +1,10 @@
+/**
+*  Sollomon Crowder
+*/
+package org.howard.edu.lsp.assignment2;
+
+public class HelloWorld1 {
+	public static void main(String[] args) {
+		System.out.println("Hello LSP2!!");
+}
+}
