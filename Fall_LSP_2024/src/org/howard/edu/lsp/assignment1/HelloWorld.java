e78a28a053b1d867142858a88d898715fb1cbfd8
sollo <sollo@localhost>
2024-09-03 13:21:31 -0500

    First message assignment 1

---
+++ Fall_LSP_2024/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+*  Sollomon Crowder
+*/
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+}
+}
+
