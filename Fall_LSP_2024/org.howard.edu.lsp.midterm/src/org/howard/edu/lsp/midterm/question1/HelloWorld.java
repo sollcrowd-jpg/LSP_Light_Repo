2fa89ccdf80d7214f3055d34ccbbe8536ac7fac4
sollo <sollo@localhost>
2024-10-15 01:54:24 -0500

    Question1

--- Fall_LSP_2024/org.howard.edu.lsp.midterm/src/org/howard/edu/lsp/midterm/question1/HelloWorld.java
+++ Fall_LSP_2024/org.howard.edu.lsp.midterm/src/org/howard/edu/lsp/midterm/question1/HelloWorld.java
@@ -1,10 +1,8 @@
 package org.howard.edu.lsp.midterm.question1;
 
 public class HelloWorld {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello World!!");
+	  }
 	}
 
-}
